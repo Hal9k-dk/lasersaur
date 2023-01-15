162a93d4ee239d6b9fc3fa90f03cb2bcbc1210a5
Torsten Martinsen <torsten@bullestock.net>
2023-01-15 11:53:07 +0100

    First attempt.

---
+++ mechv2/idler-retaining-ring.py
@@ -0,0 +1,18 @@
+import cadquery as cq
+
+id = 5
+od = 10
+len = 5
+slot_w = 1
+slot_l = 8
+
+res = (cq.Workplane("XY")
+       .circle(od/2)
+       .extrude(len)
+       .circle(id/2)
+       .cutThruAll()
+       .rect(slot_w, slot_l)
+       .cutThruAll()
+      )
+
+show_object(res)
