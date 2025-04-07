22a8bc0459b50d9e09c443568fb6772270414e7e
Torsten Martinsen <torsten@bullestock.net>
2025-04-07 18:20:59 +0200

    Add original DLC32 firmware.

---
+++ grblHAL/firmware-usb/flash_all.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+esptool.py --chip esp32 -p /dev/ttyUSB0 -b 460800 write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 bootloader.bin 0x10000 firmware.bin 0x8000 partitions.bin
