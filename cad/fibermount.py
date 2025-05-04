e3e06bf0e06d5d552c5c8feb5ed8f6dfcdf6247c
Torsten Martinsen <torsten@bullestock.net>
2025-05-04 07:23:49 +0200

    New version (DLC32).

--- cad/fibermount.py
+++ cad/fibermount.py
@@ -1,7 +1,7 @@
 import cadquery as cq
 
-w = 50
-h = 30
+w = 40
+h = 15
 th = 3
 edge = 3
 
@@ -10,12 +10,21 @@ res = (cq.Workplane("XY")
        .box(w, h, th, centered=(True, True, False))
        .edges(">Z or |Z")
        .fillet(2)
-       .workplaneFromTagged("bot")
+      )
+
+block = (cq.Workplane("XY")
        .transformed(offset=(0, 0, -2*th))
        .box(w-2*edge, h-2*edge, 2*th, centered=(True, True, False))
-       .rarray(15, 1, 3, 1)
-       .circle(1)
+       .edges("|Z")
+       .fillet(1)
+       )
+
+res = res.union(block)
+res = (res
+       .workplaneFromTagged("bot")
+       .rarray(10, 1, 4, 1)
+       .circle(1.9/2)
        .cutThruAll()
-      )
+       )
 
 show_object(res)
