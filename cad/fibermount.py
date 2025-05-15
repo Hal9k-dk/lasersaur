50f3b4525dcd6d38efc106b3db17e30e44bddcf7
Torsten Martinsen <torsten@bullestock.net>
2025-05-15 18:14:38 +0200

    More holes.

--- cad/fibermount.py
+++ cad/fibermount.py
@@ -16,14 +16,19 @@ block = (cq.Workplane("XY")
        .transformed(offset=(0, 0, -2*th))
        .box(w-2*edge, h-2*edge, 2*th, centered=(True, True, False))
        .edges("|Z")
-       .fillet(1)
+       .fillet(2)
        )
 
 res = res.union(block)
+hd = 7
 res = (res
        .workplaneFromTagged("bot")
-       .rarray(10, 1, 4, 1)
-       .circle(1.9/2)
+       .rarray(hd, 1, 5, 1)
+       .circle(1/2)
+       .cutThruAll()
+       .workplaneFromTagged("bot")
+       .transformed(offset=(2*hd, 0, 0))
+       .circle(2/2)
        .cutThruAll()
        )
 
